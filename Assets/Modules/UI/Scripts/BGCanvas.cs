ec70cb43321aecf715f987e4f84a4cdf0461e28b
Vincexodus <vincent4552@gmail.com>
2020-12-25 17:44:03 +0800

    Added cpu and ram (buggy)and some other stats(unfinished)

--- Assets/Modules/UI/Scripts/BGCanvas.cs
+++ Assets/Modules/UI/Scripts/BGCanvas.cs
@@ -1,23 +1,35 @@
 ﻿using TMPro;
 using System;
+using System.Diagnostics;
+using System.Collections;
 using UnityEngine;
 
 [ExecuteInEditMode]
 public class BGCanvas : MonoBehaviour
 {
+  // refering to resource monitor of computer **WinOs only**
+  PerformanceCounter cpuCounter =new PerformanceCounter("Processor", "% Processor Time", "_Total");
+  PerformanceCounter ramCounter = new PerformanceCounter("Memory", "Available MBytes");
   public TextMeshProUGUI datetimeText;
+  public TextMeshProUGUI performance;
+  public TextMeshProUGUI temperature;
 
   // Start is called before the first frame update
   void Start()
   {
+    print("VRAM " + SystemInfo.graphicsMemorySize + " MB");
+    print("Processor Frequency " +SystemInfo.processorFrequency + " Mhz");
+    print("RAM " + SystemInfo.systemMemorySize + " MB");
     datetimeText.richText = true;
     UpdateDateTime();
+    UpdateWeather();
   }
 
   // Update is called once per frame
   void Update()
   {
     UpdateDateTime();
+    UpdatePerformance();
   }
 
   void UpdateDateTime()
@@ -32,4 +44,20 @@ public class BGCanvas : MonoBehaviour
 
     datetimeText.text = $"{day}<sup>th</sup> <sub>of</sub> {mth} {yr}\n{hr}:{min}:{sec}";
   }
+
+  void UpdatePerformance()
+  {
+    int cpu = (int)cpuCounter.NextValue();
+    int ram = (int)ramCounter.NextValue();
+
+    performance.text = $"CPU : {cpu} %\nGPU : 0 \nRAM : {ram} %";
+  }
+
+  void UpdateWeather()
+  {
+    int temp = 30; //example have not utilize openweatherapi
+
+    temperature.text = $"{temp} °C";
+  }
+
 }
